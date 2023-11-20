52f6ea73c166885ccbcfa22f8c4865d5ecd703e1
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2023-11-20 01:06:23 -0800

    Made more improvements.

--- Example/src/main/java/org/mangorage/test/core/Example.java
+++ Example/src/main/java/org/mangorage/test/core/Example.java
@@ -1,6 +1,11 @@
 package org.mangorage.test.core;
 
 public class Example {
+
+    public Example() {
+        System.out.println("lol");
+    }
+
     public void call() {
         throw new IllegalStateException("Transformer needs to transform this");
     }
--- Example/src/main/java/org/mangorage/test/core/ExampleTransformer.java
+++ Example/src/main/java/org/mangorage/test/core/ExampleTransformer.java
@@ -15,10 +15,14 @@ public class ExampleTransformer extends ClassVisitor {
     public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
         MethodVisitor mv = super.visitMethod(access, name, desc, signature, exceptions);
 
+        System.out.println(access + " -> 1");
+        System.out.println(name+ " -> 2");
+        System.out.println(desc+ " -> 3");
+        System.out.println(signature+ " -> 4");
         // Check if the method is the 'call' method
         if ("call".equals(name) && "()V".equals(desc)) {
             // Replace the entire method with a simple print statement
-            return new MethodVisitor(Opcodes.ASM5, mv) {
+            return new MethodVisitor(Opcodes.ASM9, mv) {
                 @Override
                 public void visitCode() {
                     super.visitCode();
@@ -27,6 +31,27 @@ public class ExampleTransformer extends ClassVisitor {
                     mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "println", "(I)V", false);
                     mv.visitInsn(Opcodes.RETURN);
                 }
+            };
+        }
+
+        if ("<init>".equals(name) && "()V".equals(desc)) {
+            return new MethodVisitor(Opcodes.ASM9, mv) {
+                @Override
+                public void visitCode() {
+                    super.visitCode();
+
+                    // Original
+                    mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+                    mv.visitLdcInsn("lol c");
+                    mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
+
+                    // New Additional Code
+                    mv.visitFieldInsn(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+                    mv.visitLdcInsn("We have initiated this Example class");
+                    mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false);
+                }
+
+
             };
         }
 
